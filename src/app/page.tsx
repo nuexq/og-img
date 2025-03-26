0f5bddca6a8ceb297c941110fd657c3994698fbc
nuexq <hi.yassir.op@gmail.com>
2025-03-26 15:41:59 +0000

    no layout needed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
-export default function Home() {
+import type { Metadata } from "next";
+import Head from "next/head";
+
+export const metadata: Metadata = {
+	title: "og-img",
+	description: "og image for my site",
+};
+
+export default function Page() {
+	const title = "nuexq";
+	const encodedTitle = encodeURIComponent(title);
+
 	return (
-		<div className="flex flex-col items-center justify-center h-screen">
-			<p>Hi!</p>
+		<div>
+			<Head>
+				<meta name="og:title" content={title} />
+				<meta name="og:description" content={title} />
+				<meta
+					name="og:image"
+					content={`${
+						process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : ""
+					}/api/og?title=${encodedTitle}`}
+				/>
+			</Head>
 		</div>
 	);
 }
